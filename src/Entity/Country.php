6b9112f1c6a2da8a3550eafa4b4b611fdba3a2d7
BinGitMaker <dbinoisdevpro@gmail.com>
2022-02-02 15:32:50 +0100

    Webpack et Entity Country

---
+++ src/Entity/Country.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $duration;
+
+    #[ORM\Column(type: 'string', length: 45, nullable: true)]
+    private $hello;
+
+    #[ORM\Column(type: 'string', length: 45)]
+    private $thanku;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $picture;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $diving;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $content;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $slug;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $links;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getHello(): ?string
+    {
+        return $this->hello;
+    }
+
+    public function setHello(?string $hello): self
+    {
+        $this->hello = $hello;
+
+        return $this;
+    }
+
+    public function getThanku(): ?string
+    {
+        return $this->thanku;
+    }
+
+    public function setThanku(string $thanku): self
+    {
+        $this->thanku = $thanku;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getDiving(): ?string
+    {
+        return $this->diving;
+    }
+
+    public function setDiving(?string $diving): self
+    {
+        $this->diving = $diving;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getLinks(): ?string
+    {
+        return $this->links;
+    }
+
+    public function setLinks(?string $links): self
+    {
+        $this->links = $links;
+
+        return $this;
+    }
+}
