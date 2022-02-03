7dc404805819a981727a29e5f5d9882eea68ef97
BinGitMaker <dbinoisdevpro@gmail.com>
2022-02-03 09:12:34 +0100

    initialisation des Crud

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -3,26 +3,79 @@
 namespace App\Controller;
 
 use App\Entity\City;
+use App\Form\CityType;
 use App\Repository\CityRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/city', name: 'city_')]
-
 class CityController extends AbstractController
 {
-    #[Route('/{id}', name: 'show')]
-    public function show(
-        City $city,
-        CityRepository $cityRepository,
-        ): Response
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(CityRepository $cityRepository): Response
+    {
+        return $this->render('city/index.html.twig', [
+            'cities' => $cityRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $city = new City();
+        $form = $this->createForm(CityType::class, $city);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($city);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('city_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('city/new.html.twig', [
+            'city' => $city,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(City $city): Response
     {
         return $this->render('city/show.html.twig', [
             'city' => $city,
         ]);
-    }    
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, City $city, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CityType::class, $city);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('city_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('city/edit.html.twig', [
+            'city' => $city,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, City $city, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$city->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($city);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('city_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
